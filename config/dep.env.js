e6e19c2db59196a09d7ff380340364bcbe16f370
liumengwei <lmwffdbb@gmail.com>
2019-03-22 14:54:09 +0800

    fix(core): 修改请求地址

--- config/dep.env.js
+++ config/dep.env.js
@@ -1,10 +1,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"dep"',
-  // BASE_API: '"http://192.168.0.100:8083/admin"',
-  // BASE_API: '"http://47.100.107.204:8083/admin"',
-  // OS_API: '"http://47.100.107.204:8084/os"'
-  BASE_API: '"http://127.0.0.1:8083/admin"',
-  OS_API: '"http://127.0.0.1:8084/os"'
-  // OS_API: '"http://192.168.0.100:8083/os"'
+  BASE_API: '"http://test.fanyoutech.com:8083/admin"',
+  OS_API: '"http://test.fanyoutech.com:8084/os"'
+  // BASE_API: '"http://prod1.fanyoutech.com:8083/admin"',
+  // OS_API: '"http://prod1.fanyoutech.com:8084/os"'
 }
