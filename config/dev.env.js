218f4ca2472c938479787bbfafa6cdfae81d9244
10.12 <>
2019-05-11 16:03:09 +0800

    新增创建用户时添加页面权限功能

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,10 +1,7 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"http://127.0.0.1:8083/admin"',
-  // BASE_API: '"http://47.100.107.204:8083/admin"',
-  // BASE_API: '"http://47.98.40.106:8083/admin"',
-  // OS_API: '"http://192.168.0.100:8083/os"'
-  OS_API: '"http://127.0.0.1:8084/os"'
-  // OS_API: '"http://47.100.107.204:8084/os"'
+  BASE_API: '"http://test.fanyoutech.com:8083/admin"',
+  OS_API: '"http://test.fanyoutech.com:8084/os"'
+
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://127.0.0.1:8083/admin"',
-  OS_API: '"http://127.0.0.1:8084/os"'
-  // BASE_API: '"http://47.98.40.106:8083/admin"',
-  // OS_API: '"http://47.98.40.106:8084/os"'
+  BASE_API: '"http://prod1.fanyoutech.com:8083/admin"',
+  OS_API: '"http://prod1.fanyoutech.com:8084/os"'
 }
