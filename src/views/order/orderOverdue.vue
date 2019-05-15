49bb3902e1f99c227049e79b4ad94534edbde8ff
liumengwei <lmwffdbb@gmail.com>
2019-05-15 14:07:31 +0800

    fix(core): 逾期坏账订单状态修改

--- src/views/order/orderOverdue.vue
+++ src/views/order/orderOverdue.vue
@@ -373,12 +373,8 @@
   } from '@/utils/index'
   import DatePicker from 'vue2-datepicker'
   const statusMap = {
-    201: '已付款',
-    301: '审核通过',
-    401: '已发货',
     501: '租赁中',
-    600: '归还中',
-    900: '已完成'
+    600: '归还中'
   }
 
   export default {
