a2284a3dbb72b9c83c65d489bcb74efec3c663c3
liumengwei <lmwffdbb@gmail.com>
2020-02-10 14:03:41 +0800

    git merge

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,10 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"http://test.fanyoutech.com:8083/admin"',
-  OS_API: '"http://test.fanyoutech.com:8084/os"'
+  // BASE_API: '"http://test.fanyoutech.com:8083/admin"',
+  // OS_API: '"http://test.fanyoutech.com:8084/os"'
+  BASE_API: '"http://localhost:8083/admin"',
+  OS_API: '"http://localhost:8084/os"'
+  // BASE_API: '"http://prod1.fanyoutech.com:8083/admin"',
+  // OS_API: '"http://prod1.fanyoutech.com:8084/os"'
 }
